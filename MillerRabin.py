892c94dd3a27eda0a691e89f3c9717ed72d17ca1
Randy Steven Consuegra <rsconsuegra95@gmail.com>
2016-05-30 01:01:48 -0500

    MillerRabin.py

---
+++ MillerRabin.py
@@ -0,0 +1,58 @@
+import sys
+
+def MillerRabin(n):
+	if (n < 4759123141): 
+		semilla= [ 2, 7, 61] 
+	elif (n < 341550071728321): 
+		semilla= [2, 3, 5, 7, 11, 13, 17]
+	else: 
+		semilla =  [2, 3, 5, 7, 11, 13, 17, 19, 23]
+	d = n - 1
+	s=0
+	while ((d & 1) == 0):  
+		d >>= 1 
+		print(d)
+		s+=1
+
+	for i in range(len(semilla)):
+		a   = min(n - 2, semilla[i])
+		actual = pown(a, d, n)
+		if (actual == 1): 
+			continue
+		if (actual == n - 1): 
+			continue
+		for j in range(s):
+			actual = mul(actual, actual, n)
+			l=j
+			if (actual == n - 1):
+				break
+		if (l == s):
+			return False
+	return True
+
+def mul(a,b,mod):
+	now = 0
+	for i in range(63,-1,-1):
+		if (((a >> i) & 1) == 1):
+			break
+		for j in range(i,-1,-1):
+			now <<= 1
+			while (now > mod):
+			 now -= mod
+			if (((a >> j) & 1) == 1):
+			 now += b
+			while (now > mod):
+			 now -= mod
+	return now
+
+def pown(a,p,mod):
+	if (p == 0):
+		return 1
+	if (p % 2 == 0):
+		return pown(mul(a, a, mod), p / 2, mod)
+	return mul(pown(a, p - 1, mod), a, mod)
+	
+
+if __name__ == '__main__':
+	n = sys.argv[1]
+	print(MillerRabin(int(n)))
